f846a8f663e17029c7b2966ef70347b9e7c10f3e
Felipe Marques <felipemarquesgg@outlook.com>
2024-07-11 18:53:16 -0300

    criação do projeto

---
+++ src/main/java/com/felipe/ms_patient_labcenter/MsPatientLabcenterApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.ms_patient_labcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPatientLabcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPatientLabcenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipe/ms_patient_labcenter/MsPatientLabcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.ms_patient_labcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPatientLabcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
