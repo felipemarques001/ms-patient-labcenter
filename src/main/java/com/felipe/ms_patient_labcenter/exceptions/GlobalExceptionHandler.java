368ccf44cae36e3e268d9029cf4bb50186507683
Felipe Marques <felipemarquesgg@outlook.com>
2024-07-18 06:21:36 -0300

    feat(patient): Implementando novos tratamentos de erros em GlobalExceptionHandler

--- src/main/java/com/felipe/ms_patient_labcenter/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/felipe/ms_patient_labcenter/exceptions/GlobalExceptionHandler.java
@@ -3,15 +3,36 @@ package com.felipe.ms_patient_labcenter.exceptions;
 import com.felipe.ms_patient_labcenter.dtos.ApiGlobalResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(FieldAlreadyInUseException.class)
+    @ExceptionHandler({
+            FieldAlreadyInUseException.class,
+            ResourceNotFoundException.class,
+    })
     protected ResponseEntity<ApiGlobalResponseDTO> handleFieldAlreadyInUseException(RuntimeException ex) {
         var response = new ApiGlobalResponseDTO(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ApiGlobalResponseDTO> handleMethodArgumentNotValidException
+            (MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldNamme = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldNamme, message);
+        });
+        var response = new ApiGlobalResponseDTO(errors);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
